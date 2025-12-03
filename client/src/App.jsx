8b98e99108a2e766c4e3038416aa5c48a5ea43d1
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2025-12-03 23:22:07 +0530

    feat(routes): configure UI routes for Home, Layout, Dashboard, ResumeBuilder, Preview, and Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Preview from './pages/Preview'
+import ResumeBuilder from './pages/ResumeBuilder'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+       <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path='app' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+        </Route>
+        
+        <Route path='view/:resumeId' element={<Preview />} />
+        <Route path='login' element={<Login />} />
+
+      </Routes>
+    </>
   )
 }
 
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-  return (
-    <h1>Layout Page</h1>
-  )
+    return (
+        <div>
+            <h1>Layout Page</h1>
+            <div>
+                <Outlet />
+            </div>
+        </div>
+    )
 }
 
 export default Layout
