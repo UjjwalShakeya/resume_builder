9ba78d171155217459aad0ee6d4ce7e7befba5c2
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2025-12-06 21:14:40 +0530

    feat(ui): complete Feature component, replace SVG with Zap icon, add Title component, and update Tailwind classes - Replaced SVG icon with Zap icon from lucide-react - Created Title component inside components/home and integrated it into Feature.jsx - Passed props from Feature to Title component - Added custom Tailwind utility classes in Feature and Title components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "../components/home/Banner"
 import Hero from "../components/home/Hero"
+import Features from "../components/home/Features"
+
 const Home = () => {
     return (
         <div>
             <Banner />
             <Hero />
+            <Features />
         </div>
     )
 }
