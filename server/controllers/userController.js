9b3dc1aeae1ceff33c3ab399a874bcc74d4b898f
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2025-12-26 01:35:52 +0530

    feat(server): implement user registration controller with token generation

---
+++ server/controllers/userController.js
@@ -0,0 +1,47 @@
+// importing imporant modules
+import User from "../models/User.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const generateToken = (userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    return token;
+}
+
+// controller for user registration
+// POST: /api/users/register
+export const registerUser = async (req, res) => {
+    try {
+
+        // desctructing name email password 
+        const { name, email, password } = req.body;
+
+
+        // check if any of field is missing
+        if (!name, !email, !password) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        // check if the user already exist
+        const user = await User.findone({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // create new user
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            name, email, password: hashedPassword
+        });
+
+        // return success message
+        const token = generateToken(newUser._id);
+        newUser.password = undefined;
+
+        return res.status(201).json({ message: "User created successfully", token, user: newUser });
+
+    
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
