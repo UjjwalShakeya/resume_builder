1b0a4873bd1532e5f1202e8319b1edc65abdb898
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2025-12-30 22:07:59 +0530

    feat(navbar): integrate Redux to manage user state and complete logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import { Link, useNavigate } from "react-router-dom"
 
+
+// importing redux hooks 
+import { useDispatch, useSelector } from "react-redux";
+import {logout} from "../app/features/authSlice";
+
 const Navbar = () => {
-    const user = { name: "John Doe" };
+
+    // accessing the user via useSelector
+    const {user} = useSelector(state=> state.auth);
+    const dispatch = useDispatch();
+
     const navigate = useNavigate();
 
     // logout Handeler
     const logoutUser = () => {
         navigate("/");
+        dispatch(logout());
     };
 
     return (
