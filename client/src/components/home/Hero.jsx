57169f2292c5d4f29fad9fb881464f4969edc213
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2025-12-30 21:46:05 +0530

    feat(hero): conditionally render Login, Get Started, or Dashboard buttons based on auth state in Hero Component

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+// importing redux hooks 
+import { useSelector } from "react-redux";
+
 const Hero = () => {
+
+    const { user } = useSelector(state => state.auth);
+
     const [menuOpen, setMenuOpen] = React.useState(false);
 
     const logos = [
@@ -15,7 +21,7 @@ const Hero = () => {
     return (
         <>
             <div className="min-h-screen pb-20">
-                
+
                 {/* Navbar */}
                 <nav className="z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-40 text-sm">
                     <a href="https://prebuiltui.com">
@@ -30,12 +36,16 @@ const Hero = () => {
                     </div>
 
                     <div className="flex gap-2">
-                        <Link to='/app?state=register' className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white">
+                        <Link to='/app?state=register' className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white" hidden={user}>
                             Get started
                         </Link >
 
-                        <Link to='/app?state=login' className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" >
+                        <Link to='/app?state=login' className="hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900" hidden={user} >
                             Login
+                        </Link>
+
+                        <Link to='/app' className="hidden md:block px-6 py-2 border bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white" hidden={!user} >
+                            Dashboard
                         </Link>
                     </div>
 
